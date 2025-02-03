3dfcb3c1b1624098cc7d5ad53d774c3b9eaf77b0
mrfattah84 <148151611+mrfattah84@users.noreply.github.com>
2025-02-03 16:08:31 +0330

    add index header

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const indexRouter = require('./routes/indexRoute');
 app.use('/', indexRouter);
